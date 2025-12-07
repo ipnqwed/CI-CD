9a49afd4add48d5fc954bd539c52ec0ef8ddbaff
ipnqwed <pambewdey@gmail.com>
2025-12-08 05:28:00 +0700

    Add message env var

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello CI/CD!");
-});
+app.get("/", (req, res) => res.send(process.env.MESSAGE));
 
 app.get("/api", (req, res) => {
   res.json({ status: "OK", time: new Date() });
