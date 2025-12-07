f96c22d2ee928f984a2a6eb5942b1ed57f0826e5
ipnqwed <pambewdey@gmail.com>
2025-12-08 04:14:52 +0700

    Add /api endpoint

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
-app.get("/", (req, res) => res.send("Hello from my self-hosted CI/CD! v2"));
-app.listen(3000);
+
+app.get("/", (req, res) => {
+  res.send("Hello CI/CD!");
+});
+
 app.get("/api", (req, res) => {
   res.json({ status: "OK", time: new Date() });
 });
-app.get("/", (req, res) => res.send(process.env.MESSAGE));
+
+app.listen(3000, () => console.log("server running on port 3000"));
