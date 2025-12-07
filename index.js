6dc74a35d5db17541848a0edcd2438c6bcdbabee
ipnqwed <pambewdey@gmail.com>
2025-12-08 04:00:42 +0700

    edit index.js, and add env var in dockerfile

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-app.get("/", (req, res) => res.send("Hello from my self-hosted CI/CD!"));
+app.get("/", (req, res) => res.send("Hello from my self-hosted CI/CD! v2"));
 app.listen(3000);
 app.get("/api", (req, res) => {
   res.json({ status: "OK", time: new Date() });
